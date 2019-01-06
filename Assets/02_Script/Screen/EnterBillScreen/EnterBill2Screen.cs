6698c8f799b352b35f42e4d12395e5f300b906d4
labattbatman <alexislebel@hotmail.com>
2019-01-06 09:06:00 -0500

    Scene + quelques scripts vides

---
+++ Assets/02_Script/Screen/EnterBillScreen/EnterBill2Screen.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnterBill2Screen : MonoBehaviour
+{
+
+    public Slider m_Slider;
+    public Text m_SubAmmount;
+
+    
+    // Use this for initialization
+	void Start ()
+    {
+
+        m_Slider.onValueChanged.AddListener(SliderOnChanded);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void SliderOnChanded(float value)
+    {
+        m_SubAmmount.text = (value * 9.99).ToString("c2");
+    }
+}
